0a65ae0c4594b21e0a6786619516f8ae65e3e24c
Kirill Madorin <kirillmadorin@192.168.0.122>
2024-08-04 22:55:23 +0200

    solidity version conflicts resolved

--- packages/foundry/contracts/YourContract.sol
+++ packages/foundry/contracts/YourContract.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity ^0.8.18;
 
 // Useful for debugging. Remove when deploying to a live network.
 import "forge-std/console.sol";
